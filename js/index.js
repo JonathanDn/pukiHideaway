ffd3677001a027e53e843c277f5b965077f2b242
Robert <robert7k@gmail.com>
2016-06-14 19:14:48 +0300

    implemented selecting difficulty

--- js/index.js
+++ js/index.js
@@ -1,23 +1,49 @@
 'use strict';
 var gGameState = {};
 var gChals = [{
-		id : 'game1',
+		href : 'game1.html',
 		name : 'The Garden',
-		isSolved : true
 	}, {
-		id : 'game2',
+		href : 'game2',
 		name : 'The Forest',
-		isSolved : false
 	}, {
-		id : 'game3',
+		href : 'game3',
 		name : 'The Beach',
-		isSolved : false
 	}
 ];
 
 $(document).ready(function () {
 
 	//Load gGameState
-	
-	
+	gGameState.currChal = loadCurrChal();
+	//	gGameState.difficulty = loadGameDifficulty();
+	changeDifficulty(loadGameDifficulty());
 });
+
+function changeDifficulty(difficulty) {
+	saveGameDifficulty(difficulty);
+	
+	var $difficultyButtons = [];
+	$difficultyButtons[EASY] = $('#difficulty-easy');
+	$difficultyButtons[MEDIUM] = $('#difficulty-medium');
+	$difficultyButtons[HARD] = $('#difficulty-hard');
+	$difficultyButtons.forEach(function ($button, index) {
+		if (index === difficulty) {
+			$button.addClass('btn-primary active');
+			$button.removeClass('btn-info');
+		} else {
+			$button.removeClass('btn-primary active')
+			$button.addClass('btn-info');
+		}
+	});
+	/*	var $medium=$(#difficulty-medium);
+	var $hard=$(#difficulty-hard);
+	switch(difficulty){
+	case EASY:
+	$easy.addClass('btn-primary active');
+	$medium.removeClasss('btn-primary active');
+	$hard.removeClasss('btn-primary active');
+	break;
+	}
+	 */
+}

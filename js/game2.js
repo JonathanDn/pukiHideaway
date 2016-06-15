9ab65f7ba14c1e0bb398f61d51fb3b567ad8aeb1
jonathan <yonidn@yahoo.com>
2016-06-15 15:28:51 +0300

    "fullyFunctional row generating, yet to fix random cube bug and setTimeOut"

--- js/game2.js
+++ js/game2.js
@@ -1,22 +1,28 @@
 'use strict'
 
+var NUMS = {rows: 10,
+            cols: 7,
+            hiddenBlocks: 2
+            };
+
+
+var gTotalAttemptsCounter = 0;
+
+var gDroppedCounter = 0;
+var gCurrRow = 0;
+
+// init game 1
+$(document).ready(function () {
+    verifyChalAccess(1);
+    renderBoard(0);
+    // renderBoard(1);
+
+    // renderBoard(2);
+});
+
+
 // create the current nums.arr --> game level.
 // can be modified according to initial number / cols / rows to change DIFFICULTY
-function createBoard() {
-    for (var i = 0; i < nums.rows; i++) {
-        for (var j = 0; j < nums.cols; j++) {
-            // console.log('here');
-            // console.log('nums.initial: ', nums.initial);
-            // console.log('nums.arr[i][j]: ', nums.arr[i][j]);
-            
-            // create a 1-21 nums array.
-            nums.arr.push(nums.initial);
-            nums.initial++;
-            
-        }
-    }
-    // console.log('nums.arr: ', nums.arr);
-}
 
 function renderBoard(row) {
     var elGameContainer = document.querySelector('.gameContainer');
@@ -25,11 +31,11 @@ function renderBoard(row) {
     var strHTML = '';
     var i = row;
     strHTML += '<tr>';  
-    for (var j = 0; j < nums.cols; j++) {
+    for (var j = 0; j < NUMS.cols; j++) {
         // var counter = i * j + 1;
         // console.log('nums.arr[i][j]: ', nums.arr[i][j]);
         // added id for each cell.
-        strHTML += '<td id="' + i + '-' + j + '" class="cell">' + ((j+row*nums.cols) + 1 )+ '</td>';
+        strHTML += '<td id="' + i + '-' + j + '" class="cell">' + ((j+row*NUMS.cols) + 1 )+ '</td>';
         // counter++;
     }
     strHTML += '</tr>';
@@ -40,51 +46,38 @@ function renderBoard(row) {
     // hide 2 cells:(get them and hide them.)
 
     // take 2 random nums
-    var randNum1 = parseInt(Math.random() * nums.cols);
-    var randNum2 = parseInt(Math.random() * nums.cols);
+    var randNum1 = parseInt(Math.random() * NUMS.cols);
+    var randNum2 = parseInt(Math.random() * NUMS.cols);
     // console.log('randNum1: ', randNum1);
     
     // show 2 different empty blocks
     var elCell2Hide1 = document.getElementById(row +'-' + randNum1 );
     var elCell2Hide2 = document.getElementById(row +'-'+ randNum2 );
+    var elCells2Hide = [elCell2Hide1, elCell2Hide2];
 
-    // create the left blocks
+    // get loose block element before creating the loose blocks to drag n drop.
     var elLooseBlocks = document.querySelector('.looseBlocks');
-    elLooseBlocks.innerHTML = 
-        '<div ondrop="drop(event)" ondragover="allowDrop(event)">'
-            + ' <div class="leftBlock" draggable="true" ondragstart="drag(event)"  id="drag1">'
-                + '<span class="leftBlockNum" >' + elCell2Hide1.innerText  + '</span>'
+    // create the left blocks
+    for (var i = 0; i < elCells2Hide.length; i++) {
+        // create 1 draggable block dynamically (i+1) is 0 and than 1 to create - 2 blocks random nums.
+        elLooseBlocks.innerHTML += 
+            '<div ondrop="drop(event)" ondragover="allowDrop(event)">'
+                + ' <div class="leftBlock" draggable="true" ondragstart="drag(event)"  id="drag' + (i + 1) + '">'
+                    + '<span class="leftBlockNum" >' + elCells2Hide[i].innerText  + '</span>'
+                + '</div>'
             + '</div>'
-        + '</div>'
 
-        + '<div ondrop="drop(event)" ondragover="allowDrop(event)">'
-            + '<div class="rightBlock" draggable="true" ondragstart="drag(event)"  id="drag2">'
-                +  '<span class="rightBlockNum">' + elCell2Hide2.innerText + '</span>'
-            + '</div>'
-        + '</div>'
-
-    // console.log('elCell2Hide1: ', elCell2Hide1);
-    // console.log('elCell2Hide2: ', elCell2Hide2);
-    // hidden them succefully.
-    elCell2Hide1.innerText = ' ';
-    elCell2Hide2.innerText = ' ';
-    elCell2Hide1.style.color = 'black';
-    elCell2Hide2.style.color = 'black';
-    // add drop attributes:
-
-    // I need a div to surrond them then give him the attributes.
-    elCell2Hide1.setAttribute('ondrop', 'drop(event)')
-    elCell2Hide1.setAttribute('ondragover', 'allowDrop(event)')
-    elCell2Hide1.setAttribute('id', 'drop1');
+            // hide it's text to present a black empty box.
+            elCells2Hide[i].innerText = '';
+            elCells2Hide[i].style.color = 'black';
+            
+            // turn empty array blocks to --> droppabble. (i + 1) --> create drop1 / drop2 to determine 2 different areas to drop the dragged item.
+            elCells2Hide[i].setAttribute('ondrop', 'drop(event)')
+            elCells2Hide[i].setAttribute('ondragover', 'allowDrop(event)')
+            elCells2Hide[i].setAttribute('id', 'drop' + (i + 1));
     
-   
-    elCell2Hide2.setAttribute('ondrop', 'drop(event)');
-    elCell2Hide2.setAttribute('ondragover', 'allowDrop(event)');
-    elCell2Hide2.setAttribute('id', 'drop2');
+    }
 
-    // ondrop="drop(event)" ondragover="allowDrop(event)" id="drop1"
-    
-    
 }
 
 // DRAK & DROP:
@@ -132,10 +125,10 @@ function drop(ev) {
         // if trying to put drag(1) on drop(2) || drag(2) on drop(1) return!
         if(draggedNum.substring(4, 5) !==  ev.target.id.substring(4, 5)) {
             // up the total attempts counter
-            totalAttemptsCounter++;
+            gTotalAttemptsCounter++;
             // update the DOM total attempts counter:
             var elAttemptsCounter = document.querySelector('.attempts');
-            elAttemptsCounter.innerHTML =  totalAttemptsCounter;
+            elAttemptsCounter.innerHTML =  gTotalAttemptsCounter;
             // notfy player he is wrong
             alert("Your'e wrong, try again!")
             return;
@@ -152,11 +145,28 @@ function drop(ev) {
             ev.target.style.color = 'white';
 
             // up the total attempts counter:
-            totalAttemptsCounter++;
+            gTotalAttemptsCounter++;
             // update the DOM total attempts counter:
             var elAttemptsCounter = document.querySelector('.attempts');
-            elAttemptsCounter.innerHTML =  totalAttemptsCounter;
+            elAttemptsCounter.innerHTML =  gTotalAttemptsCounter;
 
+            // on the first drop ++ the count --> counts the draggables dropped succefully in curr row.
+            gDroppedCounter++;
+            // if the count is 2(all hidden blocks are now placed with the dragged blocks)
+            if (gDroppedCounter === NUMS.hiddenBlocks) {
+
+                // everytime I enter this if --> row num is ++
+                gCurrRow++
+                // when row number is 10 and you finish you win.
+                if (gCurrRow === NUMS.rows) {
+                    alert('You win!!!!');
+                    return;
+                }
+                // render the next row --> 0, 1, 2, 3...
+                renderBoard(gCurrRow);
+                // Reset gDropped --> for the new row level.
+                gDroppedCounter = 0;
+            }
 
             // notify the player that he is right.
             alert('right on!');
@@ -165,23 +175,5 @@ function drop(ev) {
 }
 
 
-var nums = {rows: 10,
-            cols: 7,
-            initial: 1,
-            arr: []
-            };
-
-
-var totalAttemptsCounter = 0;
-
-
-// init game 1
-$(document).ready(function () {
-    verifyChalAccess(1);
-    createBoard();
-    renderBoard(0);
-    // renderBoard(1);
-    // renderBoard(2);
-});
 
 

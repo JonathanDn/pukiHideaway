a8d9419ca753569d60efeabfe464a18f4fb983fa
Robert <robert7k@gmail.com>
2016-06-15 19:18:02 +0300

    game3 is now a fully functional drag&drop decisions demo

--- js/game3.js
+++ js/game3.js
@@ -1,1 +1,11 @@
 'use strict';
+$(document).ready(function () {
+	//    verifyChalAccess(2);
+	//    renderBoard(0);
+	$("#drag1").draggable();
+	$("#drag2").draggable();
+	
+	$('#drop1').droppable({accept:'#drag1'});
+	$('#drop2').droppable({accept:'#drag2'});
+	$('#drop3').droppable();
+});

11a6a634157c8932dbf7b58f98f15c86f2e1119e
Robert <robert7k@gmail.com>
2016-06-15 16:50:30 +0300

    updated localstorage.js to make sure not to decrease current chal created files for game3

--- js/localStorage.js
+++ js/localStorage.js
@@ -6,35 +6,41 @@ var HARD = 2;
 //returns current difficulty (from localstorage)
 function loadGameDifficulty() {
 	var difficulty = localStorage.getItem('pukisHideawayByRobertAndJonnyDifficulty');
-	if (difficulty === null) difficulty = EASY;
+	if (difficulty === null) {
+		difficulty = EASY;
+	}
 	return +difficulty;
 }
 
 //returns current unlocked chal
 function loadCurrChal() {
-	var currChal=localStorage.getItem('pukisHideawayByRobertAndJonnyCurrChal');
-	if (currChal===null) currChal=0;
+	var currChal = localStorage.getItem('pukisHideawayByRobertAndJonnyCurrChal');
+	if (currChal === null) {
+		currChal = 0;
+	}
 	return +currChal;
 }
 
-//updates difficulty (to be used by the main menu)
+//updates difficulty (to be used by the main menu when the user changes difficulty)
 function saveGameDifficulty(difficulty) {
 	localStorage.pukisHideawayByRobertAndJonnyDifficulty = difficulty;
 }
 
 //updates current unlocked chal (to be used when the user finishes a chal)
 function saveCurrChal(currChal) {
-	localStorage.pukisHideawayByRobertAndJonnyCurrChal = currChal;
+	if (loadCurrChal() < currChal) {
+		localStorage.pukisHideawayByRobertAndJonnyCurrChal = currChal;
+	}
 }
 
 //checks if the user is allowed to play this chal, if not redirects to the main menu
 //to be used onload of every chal
-function verifyChalAccess(chal){
-	var currChal=loadCurrChal();
-//	console.log('chal:',chal,'currChal:',currChal,'currChal<chal:',currChal<chal)
-	if (currChal<chal) {
-		window.location='index.html';
-//		return ('acting');
+function verifyChalAccess(chal) {
+	var currChal = loadCurrChal();
+	//	console.log('chal:',chal,'currChal:',currChal,'currChal<chal:',currChal<chal)
+	if (currChal < chal) {
+		window.location = 'index.html';
+		//		return ('acting');
 	}
-//	return ('access granted');
+	//	return ('access granted');
 }

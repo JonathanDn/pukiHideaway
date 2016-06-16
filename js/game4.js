a8bf3ea78d0d7e8e8b90e6e11fce2c0c28121f8f
jonathan <yonidn@yahoo.com>
2016-06-16 17:45:40 +0300

    "proportional hippos on mobile"

--- js/game4.js
+++ js/game4.js
@@ -1,1 +1,41 @@
 'use strict';
+
+var quests = [{id: 1,
+               image: 'img/hippo.png',
+               answers: [1, 2, 3, 4, 5],
+               correctOptIndex: 1,
+               isCorrect: false
+               },
+               {id: 2,
+               image: 'img/elephant.png',
+               answers: [1, 2, 3, 4, 5],
+               correctOptIndex: 1,
+               isCorrect: false
+               },
+               {id: 3,
+               image: 'img/butterfly.png',
+               answers: [1, 2, 3, 4, 5],
+               correctOptIndex: 1,
+               isCorrect: false
+               },
+               {id: 4,
+               image: 'img/turtle.png',
+               answers: [1, 2, 3, 4, 5],
+               correctOptIndex: 1,
+               isCorrect: false
+               },
+               {id: 5,
+               image: 'img/panda.png',
+               answers: [1, 2, 3, 4, 5],
+               correctOptIndex: 1,
+               isCorrect: false
+               }];
+
+
+function getAnimalLocation() {
+    var elLocation = document.querySelectorAll('.animal');
+    console.log('elLocation: ', elLocation);
+    
+};
+
+getAnimalLocation();

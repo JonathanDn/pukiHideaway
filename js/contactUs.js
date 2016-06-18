0127f552edca798c432c491bf520756ef849dce6
jonathan <yonidn@yahoo.com>
2016-06-18 23:09:55 +0300

    storeContacts - local storage improved

--- js/contactUs.js
+++ js/contactUs.js
@@ -4,14 +4,21 @@
 // store all the contact details on click of submit
 function store() {
 	var inputEmail = document.getElementById('email');
-	localStorage.setItem('email', inputEmail.value);
 	var inputPhone = document.getElementById('phone');
-	localStorage.setItem('phone', inputPhone.value);
 	var inputFName = document.getElementById('fname');
-	localStorage.setItem('fname', inputFname.value);
 	var inputLName = document.getElementById('lname');	
-	localStorage.setItem('lname', inputLname.value);
 	var inputMessage = document.getElementById('message');
-	localStorage.setItem('message', inputMessage.value);				
+
+	var contact = {	'email': inputEmail,
+					'phone': inputPhone,
+					'fname': inputFName,
+					'lname': inputLName,
+					'message': inputMessage }
+
+	var contacts = [];
+	contacts.push(contact);
+	
+	// store contacts array to local storage
+	localStorage.setItem('contacts', JSON.stringify(contacts));				
 	
 }
